f4fcfb67e35666abfad6a5649919e9e7d4851575
nellygreen <nellygreen.cg@gmail.com>
2024-12-28 21:25:30 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -89,6 +89,19 @@ onMounted(() => {
       </form>
     </section>
 
+    <section class="todo-list">
+
+      <h3>TODO LIST</h3>
+      <div class="list">
+
+        <div v-for="todo in todos_asc">
+
+        </div>
+
+      </div>
+
+    </section>
+
   </main>
 
 </template>
