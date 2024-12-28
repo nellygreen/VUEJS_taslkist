ddc7b26c7f6ae267e0dc923c22ac76baa3c8203c
nellygreen <nellygreen.cg@gmail.com>
2024-12-28 03:44:06 +0100

    First

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script setup>
+//setting up the local storage
+import { ref, onMounted, computed, watch } from 'vue'
+
+//setup the global variables
+const todos = ([]) 
+const name = ref ('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+//filtering system setup
+const todos_asc = computed(() => todos.value.sort((a,  b) => {
+  return a.createdAt - b.createdAt
+}))
+
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return  
+  }
+}
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+
+</script>
+
+
+<template>
+
+  <maim class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name"/>
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TASK</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your list today?</h4>
+        <input 
+          type="text" 
+          placeholder="e.g. make some money" 
+          v-model="input_content" />
+          <h4>Pick a category</h4>
+
+          <div class="options">
+            <label>
+              <input 
+                type="radio" 
+                name="category"
+                value="business"
+                v-model="input_category"
+                />
+                <span class="bubble business"></span>
+                <div>Business</div>
+            </label>
+
+            <label>
+              <input 
+                type="radio" 
+                name="category"
+                value="personal"
+                v-model="input_category"
+                />
+                <span class="bubble personal"></span>
+                <div>Personal</div>
+            </label>
+          </div>
+
+          <input type="submit" value="Add Task" />
+      </form>
+    </section>
+
+  </maim>
+
+</template>
